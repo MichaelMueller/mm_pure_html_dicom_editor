17b3b6c6593db3cb5bae1425730b37e308e33319
Michael Mueller <mueller@mbits.info>
2022-11-08 18:17:58 +0100

    Checkpoint commit as of 2022-11-08-18-17-58.

--- public/app.js
+++ public/app.js
@@ -8,10 +8,18 @@ class App
     ///  
     constructor()
     {
+        this._delimiter = "/";
         ///
         /// @var object
         ///
-        this._data = {};
+        this[this._delimiter] = {};
+    }
+    ///
+    /// @return string
+    ///  
+    delimiter()
+    {
+        return this._delimiter;
     }
     ///        
     /// @param object changes
@@ -27,9 +35,14 @@ class App
     ///  
     get( path )
     {
+        if( path == null || path == this._delimiter )
+            path = "";
+
+        let keys = String(path).split(this._delimiter);
+        keys[0] = this._delimiter;
+
         // recursively get the value
-        let keys = String(path).split(".");
-        let curr_object = this._data;
+        let curr_object = this;
         while( keys.length > 0 )
         {
             let key = keys.shift();
@@ -58,8 +71,16 @@ class App
     ///  
     _set( path, value, validate=false )
     {
-        // convert to changes object       
-        let keys = String(path).split(".");
+        if( path == null || path == this._delimiter || path == "")
+        {
+            console.error(`Invalid path ${path}`);
+            return this;
+        }
+
+        let keys = String(path).split(this._delimiter);
+        keys.shift();
+
+        // recursively build a change object
         let changes = {};
         let curr_object = changes;
         while( keys.length > 0 )
@@ -85,8 +106,9 @@ class App
         let updated_items = {};
         let prev_items = {};
         let parent_path = null;
+        changes = { [this._delimiter]: changes };
 
-        this._recursive_update( changes, validate, this._data, updated_items, prev_items, parent_path );        
+        this._recursive_update( changes, validate, this, updated_items, prev_items, parent_path );        
         this._on_updated( updated_items, prev_items );
     }
     ///        
@@ -111,7 +133,7 @@ class App
         let changed = false;
         for( let key of keys )
         {
-            let curr_path = ( parent_path ? parent_path + "." : "" ) + String(key);
+            let curr_path = ( parent_path == null ? "" : ( parent_path == this._delimiter ? parent_path : parent_path + this._delimiter) ) + String(key);
             // use filter if needed
             let updated_value = this._filter_on_update( curr_path, changes[key], true );
             let curr_value = key in target ? target[key] : null;
@@ -178,7 +200,7 @@ class App
         {
             let copy = Array.isArray(obj) ? [] : {};
             for( let key in obj )
-                copy[key] = deep_copy( obj[key] );
+                copy[key] = App.deep_copy( obj[key] );
             return copy;                        
         }
         else
@@ -188,7 +210,182 @@ class App
 ///
 /// Mirrors changes to the DOM and vice versa
 ///  
-class HtmlApp
+class HtmlApp extends App
 {
+    ///
+    /// @return this
+    ///  
+    constructor()
+    {
+        super();
+        this._dom_loaded = false;
+        this._cached_updates = [];
+        this._cached_validation_flags = [];
+        this._active_element = null;
+        this._init();
+    }
+    ///
+    /// @return void
+    ///  
+    _init()
+    {            
+        document.addEventListener("DOMContentLoaded", function(event) 
+        {               
+            this._dom_loaded = true;  
 
+            document.addEventListener("change", function(event) 
+            { 
+                let elem = event.target;
+                if( "bindValue" in elem.dataset )
+                {
+                    let path = elem.getAttribute("name");
+                    let value = null;
+                    if( elem.tagName.toLowerCase() == "input" && elem.getAttribute("type").toLowerCase() == "file" )
+                    {
+                        path = path + ".files";
+                        value = event.target.files;
+                    }                        
+                    else if("options" in event.target)
+                    {
+                        const selectedValues = [];
+                        for (let i = 0; i < event.target.options.length; i++)                             
+                            if (event.target.options[i].selected)                                 
+                                selectedValues.push(event.target.options[i].value);
+                        value = selectedValues;
+                    }
+                    else
+                        value = event.target.value;
+                    this._active_element = elem;
+                    this.set( path, value );
+                    this._active_element = null;
+                }
+
+            }.bind(this));
+
+            // apply cache
+            for( let i=0; i<this._cached_updates.length; ++i )
+                this._update( this._cached_updates[i], this._cached_validation_flags[i] );
+
+            this._cached_updates = null;
+            this._cached_validation_flags = null;
+        }.bind(this));
+    }
+    ///        
+    /// @param object changes
+    /// @param bool validate
+    /// @return this
+    ///  
+    _update( changes, validate=false ) 
+    {
+        if(this._dom_loaded == false)
+        {
+            this._cached_updates.push( App.deep_copy( changes ) );
+            this._cached_validation_flags.push( validate );
+            return this;
+        }
+        else
+            return super._update( changes, validate );
+    }
+    ///
+    /// @param object updated_items all changes as path -> value items
+    /// @param object prev_values
+    /// @return void
+    ///  
+    _on_updated( updated_items, prev_items ) 
+    {
+        let updated_paths = Object.keys(updated_items);
+        updated_paths.reverse();
+        for( let path of updated_paths )
+        {
+            let value = updated_items[path];
+            //let prev_value = updated_items[path];
+            // get the value
+            let bool_value = value == null ? false : Boolean(value);
+            let str_value = value == null ? "" : String(value);
+            if( Array.isArray(value) && value.length == 0 )
+                bool_value = false;
+
+            // the general exist binding (remove element if)
+            /*let elements = document.querySelectorAll(`[data-bind-exist="${path}"]`);
+            for( let i=0; i < elements.length; ++ i)
+            {
+                let elem = elements.item(i);
+                if( bool_value == false )
+                    elem.remove();           
+            }*/
+            let elements = null;
+            // bind whole object to elements
+            if( value != null && typeof value == "object" )
+            {                  
+                let child_object = value;      
+                elements = document.querySelectorAll(`[data-bind-object="${path}"]`);
+                for( let i=0; i < elements.length; ++ i)
+                {                    
+                    let elem = elements.item(i);
+                    let first_child = elem.children[0];
+                    first_child.style.display = "none";
+                    let first_child_html = new XMLSerializer().serializeToString(first_child);
+                    first_child_html = first_child_html.replaceAll(' xmlns="http://www.w3.org/1999/xhtml"', '');
+                    
+                    elem.innerHTML = first_child_html;
+                    for( let key in child_object )
+                    {
+                        let item_elem_html = first_child_html.replaceAll('$key$', key);
+                        item_elem_html = item_elem_html.replaceAll('display: none;', "");
+                        item_elem_html = item_elem_html.replaceAll(' style=""', "");
+                        elem.innerHTML += item_elem_html;
+                    }
+                }  
+            }
+
+            // bind value to form elements
+            elements = document.querySelectorAll(`[name="${path}"][data-bind-value]`);
+            for( let i=0; i < elements.length; ++ i)
+            {
+                let elem = elements.item(i);
+                if( elem != this._active_element && ( elem.getAttribute("type") != "file" || str_value == "") )
+                    elem.value = str_value;    
+            }
+
+            // bind html content
+            elements = document.querySelectorAll(`[data-bind-html="${path}"]`);
+            for( let i=0; i < elements.length; ++ i)
+            {
+                let elem = elements.item(i);
+                elem.innerHTML = str_value;    
+                if( "htmlSuffix" in elem.dataset )
+                    elem.innerHTML += elem.dataset.htmlSuffix;             
+            }
+
+            // bind css display property
+            elements = document.querySelectorAll(`[data-bind-display="${path}"]`);
+            for( let i=0; i < elements.length; ++ i)
+            {
+                let elem = elements.item(i);
+                let display_type = "displayFlex" in elem.dataset ? "flex" : "block";
+                elem.style.display = ("bindDisplayInvert" in elem.dataset ? ! bool_value : bool_value) ? display_type : "none";                        
+            }
+
+            // bind css visibility property
+            elements = document.querySelectorAll(`[data-bind-visibility="${path}"]`);
+            for( let i=0; i < elements.length; ++ i)
+            {
+                let elem = elements.item(i);
+                elem.style.visibility = ("bindVisibilityInvert" in elem.dataset ? ! bool_value : bool_value) ? "visible" : "hidden";                        
+            }
+
+            // bind css classes to an element
+            elements = document.querySelectorAll(`[data-bind-css-class="${path}"]`);
+            for( let i=0; i < elements.length; ++ i)
+            {
+                let elem = elements.item(i);
+                let cssClass = elem.dataset.cssClass;
+                if( bool_value )
+                    elem.classList.add(cssClass);              
+                else
+                    elem.classList.remove(cssClass);
+            }
+        }        
+    }
+    
 }
